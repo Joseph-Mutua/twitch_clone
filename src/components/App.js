d5dee20d11f561ebe6afe97cac9ecc4ca0055651
Joseph Mutua <Mutuaj793@gmail.com>
2020-12-01 19:55:56 +0300

    Component Scaffolding

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,24 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+
+const PageOne = () => {
+    return <div>Page One</div>
+}
+
+const PageTwo = () => {
+  return <div>Page Two</div>;
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Route path="/" exact component={PageOne} />
+        <Route path="/pagetwo" exact component={PageTwo} />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;

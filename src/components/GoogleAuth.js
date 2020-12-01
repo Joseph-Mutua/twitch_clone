52745c66ecafd9c45c4dc2353b7adc6b91d4247b
Joseph Mutua <Mutuaj793@gmail.com>
2020-12-01 22:10:00 +0300

    Rendering authentication status

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,17 +1,35 @@
 import React, { Component } from "react";
 
 class GoogleAuth extends Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({
-        clientId:
-          "532681052447 - onl7stemqim1hagoqvfdvrpe9ts5s7sq.apps.googleusercontent.com;",
-        scope: "email",
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            "532681052447 - onl7stemqim1hagoqvfdvrpe9ts5s7sq.apps.googleusercontent.com;",
+          scope: "email",
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
     });
   }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I dont know if we're signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am Signed In!</div>;
+    } else {
+      return <div>I am not Signed in!</div>;
+    }
+  }
+
   render() {
-    return <div>Google Auth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 

4beef1ba4a3883228f0c6179e0809281ed2719dd
Joseph Mutua <Mutuaj793@gmail.com>
2020-12-06 18:58:12 +0300

    creating a flvplayer

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -10,7 +10,14 @@ class StreamShow extends Component {
   }
 
   componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+    const { id } = this.props.match.params;
+    this.props.fetchStream(id);
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}`,
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   render() {
@@ -22,7 +29,7 @@ class StreamShow extends Component {
 
     return (
       <div>
-        <video ref={this.videoRef} style={{width: "100%"}} controls={true} />
+        <video ref={this.videoRef} style={{ width: "100%" }} controls={true} />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
